d0dc4c2895cc95f22590be16275eb0e9c8046a2b
xiezhigui <421548825@qq.com>
2019-10-24 15:43:39 +0800

    修改提交

---
+++ service-eureka/src/main/java/com/baidu/eureka/config/MyConfig.java
@@ -0,0 +1,12 @@
+package com.baidu.eureka.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @Auther: XieZhiGui
+ * @Date: 2019-10-24
+ * @Description: com.baidu.eureka.config
+ * @version: 1.0
+ */
+public class MyConfig {
+}
